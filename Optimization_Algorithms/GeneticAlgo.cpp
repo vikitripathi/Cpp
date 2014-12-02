c5576a0e10bb596a38db95a65f96caf64921fc94
vikitripathi <abhishek09011993@gmail.com>
2014-12-02 07:28:14 +0530

    feat: Algo skeleton setup

---
+++ Optimization_Algorithms/GeneticAlgo.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <ctime>
+#include <stdio.h> /* printf, scanf, puts, NULL */
+#include <stdlib.h> /* srand, rand */
+#include <time.h> /* time */
+#include <cmath>
+
+using namespace std;
+
+/*
+The goal of any optimization algorithm is to find a set of inputs that minimizes the cost function, so the
+cost function has to return a value that represents how bad a solution is. There is no particular scale forbadness; 
+the only requirement is that the function returns larger values for worse solutions
+ */
+
+
+/*
+Now as the cost function has been listed , goal is to min cost by choosing correct set of numbers
+ */
+
+/*
+random searching and hill climbing can get stuck in local  minima
+Genetics algorithms are way to avoid  getting struck in local minima and find the best overall minima ,called the
+global minimum, which is what optimization algorithms are ultimately supposed to find.
+ */
+
+int geneticOptimize(){
+	
+
+}
+
+
+
+int main ()
+{
+
+}
